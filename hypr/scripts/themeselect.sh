c214bc1a1061a7241f9e048caf6ea54f2bd277d1
Towhidul Islam <67688815+TowhidMolla-Dev@users.noreply.github.com>
2023-09-06 15:06:35 +0600

    Add files via upload

--- hypr/scripts/themeselect.sh
+++ hypr/scripts/themeselect.sh
@@ -1,49 +1,38 @@
 #!/usr/bin/env sh
 
-## set variables ##
+# set variables
 BaseDir=`dirname $(realpath $0)`
 ThemeCtl="$HOME/.config/swww/wall.ctl"
-ThumbDir="$HOME/.config/swww/Themes-Ctl"
+CacheDir="$HOME/.config/swww/.cache"
 RofiConf="$HOME/.config/rofi/themeselect.rasi"
 ThemeSet="$HOME/.config/hypr/themes/theme.conf"
 
 
-## show and apply theme ##
-if [ -z "$1" ] ; then
+# scale for monitor x res
+x_monres=`cat /sys/class/drm/*/modes | head -1 | cut -d 'x' -f 1`
+x_monres=$(( x_monres*17/100 ))
 
-    hypr_border=`awk -F '=' '{if($1~" rounding ") print $2}' $ThemeSet | sed 's/ //g'`
-    elem_border=$(( hypr_border * 5 ))
-    icon_border=$(( elem_border - 5 ))
-    r_override="element {border-radius: ${elem_border}px;} element-icon {border-radius: ${icon_border}px;}"
 
-    ThemeSel=$(cat $ThemeCtl | while read line
-    do
-        thm=`echo $line | cut -d '|' -f 2`
-        wal=`echo $line | cut -d '|' -f 3`
-        echo -en "$thm\x00icon\x1f$ThumbDir/${thm}.png\n"
-    done | rofi -dmenu -theme-str "${r_override}" -config $RofiConf)
+# set rofi override
+hypr_border=`awk -F '=' '{if($1~" rounding ") print $2}' $ThemeSet | sed 's/ //g'`
+elem_border=$(( hypr_border * 5 ))
+icon_border=$(( elem_border - 5 ))
+r_override="element{border-radius:${elem_border}px;} element-icon{border-radius:${icon_border}px;size:${x_monres}px;}"
 
-    if [ ! -z $ThemeSel ] ; then
-        ${BaseDir}/themeswitch.sh -s $ThemeSel
-    fi
 
-## regenerate thumbnails ##
-elif [ "$1" == "T" ] ; then
+# launch rofi menu
+ThemeSel=$( cat $ThemeCtl | while read line
+do
+    thm=`echo $line | cut -d '|' -f 2`
+    wal=`echo $line | awk -F '/' '{print $NF}'`
+    #echo $thm $wal
+    echo -en "$thm\x00icon\x1f$CacheDir/${thm}/${wal}\n"
+done | rofi -dmenu -theme-str "${r_override}" -config $RofiConf)
 
-    echo "refreshing thumbnails..."
-    cat $ThemeCtl | while read line
-    do
-        thm=`echo $line | cut -d '|' -f 2`
-        wal=`echo $line | cut -d '|' -f 3`
-        wal=`eval echo $wal`
 
-        echo "croping image from wallpaper $ThumbDir/${thm}.png..."
-        convert $wal -thumbnail 500x500^ -gravity center -extent 500x500 $ThumbDir/${thm}.png
-        #convert $wal -gravity Center -crop 1080x1080+0+0 $ThumbDir/${thm}.png
-        #echo "applying rounded corner mask and generating $ThumbDir/${thm}.png..."
-        #convert -size 1080x1080 xc:none -draw "roundrectangle 0,0,1080,1080,80,80" $ThumbDir/roundedmask.png
-        #convert $ThumbDir/${thm}_tmp.png -matte $ThumbDir/roundedmask.png -compose DstIn -composite $ThumbDir/${thm}.png
-    done
-
+# apply theme
+if [ ! -z $ThemeSel ] ; then
+    ${BaseDir}/themeswitch.sh -s $ThemeSel
 fi
+
 
